a5fefb675c49a181ddf2640b8582817639a9d39c
jeff murphy <jcmurphy@jeffmurphy.org>
2015-12-04 11:56:06 -0600

    latest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203230536) do
+ActiveRecord::Schema.define(version: 20151204175019) do
 
   create_table "domains", force: :cascade do |t|
     t.string   "domain"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20151203230536) do
     t.string   "domain"
     t.datetime "posted_at"
     t.string   "notetype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
